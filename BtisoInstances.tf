6da4adce887e1f46fdd15795ffa944cd05ab4b1d
enanosca <e.odnavres@gmail.com>
2020-04-06 23:51:47 -0500

    Secuirity Group - declaration changed.

--- BtisoInstances.tf
+++ BtisoInstances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "BitsoEC2" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.BitsoPublicSubnet.id}"
-  vpc_security_group_ids = "${aws_security_group.BitsoSG.id}"
+  vpc_security_group_ids = ["${aws_security_group.BitsoSG.id}"]
   user_data = <<-EOF
               #!/bin/bash
               echo "Bitso Challenge || Luis Rios" > index.html
