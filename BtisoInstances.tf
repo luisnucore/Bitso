a2c6980dc6b7e3c9eb359851dc7a53fb1e44b42d
enanosca <e.odnavres@gmail.com>
2020-04-07 02:05:34 -0500

    adding monitoring on DataDog

--- BtisoInstances.tf
+++ BtisoInstances.tf
@@ -3,24 +3,11 @@ resource "aws_instance" "BitsoEC2" {
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.BitsoPublicSubnet.id}"
   vpc_security_group_ids = ["${aws_security_group.BitsoSG.id}"]
-  key_name = "terrraform-oregon"
   user_data = <<-EOF
               #!/bin/bash
               echo "Bitso Challenge || Luis Rios" > index.html
               nohup busybox httpd -f -p 8080 &
-              EOF
-
-  connection {
-                  host          = "${aws_instance.BitsoEC2.public_ip}"
-                  type          = "ssh"
-                  user          = "ec2-user"
-                  private_key   = "${var.pvt_key}"
-                  timeout       = "1m"
-                  agent         = false
-              }
-  provisioner "remote-exec" {
-                  inline = [ DD_API_KEY=<user_api_key> bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)",
-
-                  ]
+              DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=9c69c53d75900006ea86a2870429b15c bash -c " $(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+	      EOF
 
 }
