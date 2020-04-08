a23ce901ba237ddb91a2a688f8055e9787bd818e
enanosca <e.odnavres@gmail.com>
2020-04-07 21:28:18 -0500

    e

--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "us-west-2"
-  access_key = "${var.id}"
-  secret_key = "${var.key}"
-}
--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "id" {} #these 2 variables are proovided  with therraform execution
-variable "key"{}
-
-output "instance_ips" {
-  value = ["${aws_instance.BitsoEC2.public_ip}"]
-}
